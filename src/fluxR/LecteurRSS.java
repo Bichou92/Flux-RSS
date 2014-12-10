496228bc09713478295c858b9b8d6e4f3b5d172d
ebischoff <ebischoff@192.168.10.99>
2014-12-10 17:34:57 +0100

    Last Version

--- src/fluxR/LecteurRSS.java
+++ src/fluxR/LecteurRSS.java
@@ -18,53 +18,7 @@ import org.xml.sax.SAXException;
 
 public class LecteurRSS 
 {
-
-    //Parser avec URL du flux RSS avec parse(@param envoiUrl)
-    public void parse(String envoiUrl) 
-    {
-        try 
-        {
-            DocumentBuilder document = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            URL url = new URL(envoiUrl);
-            Document doc = document.parse(url.openStream());
-            NodeList nodes = null;
-            Element element = null;
-            
-            //Recupère le titre et la date de la dernière publication du flux
-            nodes = doc.getElementsByTagName("title");
-            Node contenu = doc.getDocumentElement();
-            System.out.println("Flux RSS: " + this.recupNoeud(contenu, "channel|title"));
-            System.out.println("Date de la dernière publication: " + this.recupNoeud(contenu, "channel|lastBuildDate"));
-            System.out.println();
-            
-            
-            //Recupère chaque élement du flux RSS et l'affiche
-            nodes = doc.getElementsByTagName("item");
-            for (int i = 0; i < nodes.getLength(); i++) 
-            {
-                element = (Element) nodes.item(i);
-                System.out.println("Titre: " + recupNoeud(element, "title"));
-                System.out.println("Lien: " + recupNoeud(element, "link"));
-                System.out.println("Date: " + recupNoeud(element, "pubDate"));
-                System.out.println("Description: " + recupNoeud(element, "description"));
-                System.out.println();
-            }
-          
-        } catch (SAXException ex) 
-          {
-            Logger.getLogger(LecteurRSS.class.getName()).log(Level.SEVERE, null, ex);
-          } 
-          catch (IOException ex) 
-          {
-            Logger.getLogger(LecteurRSS.class.getName()).log(Level.SEVERE, null, ex);
-          } 
-          catch (ParserConfigurationException ex) 
-          {
-            Logger.getLogger(LecteurRSS.class.getName()).log(Level.SEVERE, null, ex);
-          }
-    }
-
-    //Récupère tous les éléments dans un tableau de noeuds et récupère ce qu'il contient (string)
+	//Récupère tous les éléments dans un tableau de noeuds et récupère ce qu'il contient (string)
     public String recupNoeud(Node noeud, String chemin) 
     {
 
@@ -114,6 +68,51 @@ public class LecteurRSS
         return null;
     }
     
+    //Parser avec URL du flux RSS avec parse(@param envoiUrl)
+    public void parse(String envoiUrl) 
+    {
+        try 
+        {
+            DocumentBuilder document = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            URL url = new URL(envoiUrl);
+            Document doc = document.parse(url.openStream());
+            NodeList nodes = null;
+            Element element = null;
+            
+            //Recupère le titre et la date de la dernière publication du flux
+            nodes = doc.getElementsByTagName("title");
+            Node contenu = doc.getDocumentElement();
+            System.out.println("Flux RSS: " + this.recupNoeud(contenu, "channel|title"));
+            System.out.println("Date de la dernière publication: " + this.recupNoeud(contenu, "channel|lastBuildDate"));
+            System.out.println();
+            
+            
+            //Recupère chaque élement du flux RSS et l'affiche
+            nodes = doc.getElementsByTagName("item");
+            for (int i = 0; i < nodes.getLength(); i++) 
+            {
+                element = (Element) nodes.item(i);
+                System.out.println("Titre: " + recupNoeud(element, "title"));
+                System.out.println("Lien: " + recupNoeud(element, "link"));
+                System.out.println("Date: " + recupNoeud(element, "pubDate"));
+                System.out.println("Description: " + recupNoeud(element, "description"));
+                System.out.println();
+            }
+          
+        } catch (SAXException ex) 
+          {
+            Logger.getLogger(LecteurRSS.class.getName()).log(Level.SEVERE, null, ex);
+          } 
+          catch (IOException ex) 
+          {
+            Logger.getLogger(LecteurRSS.class.getName()).log(Level.SEVERE, null, ex);
+          } 
+          catch (ParserConfigurationException ex) 
+          {
+            Logger.getLogger(LecteurRSS.class.getName()).log(Level.SEVERE, null, ex);
+          }
+    }
+
     //méthode retournant le xml parsé et prêt à l'emploi
     public static void afficherRss()
     {
